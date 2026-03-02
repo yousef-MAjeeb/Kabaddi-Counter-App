ce56f1b3c46386f068d570302afabea5bf422164
idiot-Dev <3amkhero@gmail.com>
2026-03-02 17:22:17 +0200

    update code to use viewBinding

--- app/src/main/java/com/example/kabaddicounter/MainActivity.kt
+++ app/src/main/java/com/example/kabaddicounter/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import com.example.kabaddicounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,40 +18,41 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         //team A
-        val scoreA: TextView = findViewById(R.id.tvTeamAScore)
-        val addOneBtnA: Button = findViewById(R.id.add1BtnA)
-        val addTwoBtnA: Button = findViewById(R.id.add2BtnA)
-        scoreA.text = "0"
-        addOneBtnA.setOnClickListener {
+//        binding.tvTeamAScore.text = viewModel.scoreA.toString()
+
+        binding.add1BtnA.setOnClickListener {
             viewModel.incrementScoreByOne(true)
-            scoreA.text = viewModel.scoreA.toString()
+            binding.tvTeamAScore.text = viewModel.scoreA.toString()
         }
-        addTwoBtnA.setOnClickListener {
+
+        binding.add2BtnA.setOnClickListener {
             viewModel.incrementScoreByTwo(true)
-            scoreA.text = viewModel.scoreA.toString()
+            binding.tvTeamAScore.text = viewModel.scoreA.toString()
         }
 
         //team B
-        val scoreB: TextView = findViewById(R.id.tvTeamBScore)
-        val addOneBtnB: Button = findViewById(R.id.add1BtnB)
-        val addTwoBtnB: Button = findViewById(R.id.add2BtnB)
-        scoreB.text = "0"
-        addOneBtnB.setOnClickListener {
+//        binding.tvTeamBScore.text = viewModel.scoreB.toString()
+
+        binding.add1BtnB.setOnClickListener {
             viewModel.incrementScoreByOne(false)
-            scoreB.text = viewModel.scoreB.toString()
+            binding.tvTeamBScore.text = viewModel.scoreB.toString()
+
         }
-        addTwoBtnB.setOnClickListener {
+
+        binding.add2BtnB.setOnClickListener {
             viewModel.incrementScoreByTwo(false)
-            scoreB.text = viewModel.scoreB.toString()
+            binding.tvTeamBScore.text = viewModel.scoreB.toString()
+
         }
 
         val resetBtn: Button = findViewById(R.id.resetBtn)
         resetBtn.setOnClickListener {
             viewModel.resetScore(true)
-            scoreA.text = "0"
-            scoreB.text = "0"
+            binding.tvTeamBScore.text = viewModel.scoreB.toString()
+            binding.tvTeamBScore.text = viewModel.scoreB.toString()
         }
     }
 }
